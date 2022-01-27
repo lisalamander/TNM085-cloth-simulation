1692f6bc16923d4c778e27ae5399b07b69720723
Alfred Rundqvist <alfred.rundqvist@gmail.com>
2022-01-27 21:00:42 +0100

    Added algorithm for i x j masses, but very slow. Added shear springs aswell

---
+++ MATLAB/large.m
@@ -0,0 +1,160 @@
+clear
+clf
+hold on
+xlim([0 1])
+ylim([0 1])
+
+rows = 3;
+cols = 3;
+
+f_g = [0 9.82];
+h = 0.01;
+% Masses
+m = 1;
+% Spring constant
+k = 30;
+
+% Damping constant
+c = 5;
+% Initial positions and velocities
+for i = 1:rows
+    for j = 1:cols
+        node(i,j).p = [((j-1)/(cols-1))/2+0.25 ((i-1)/(cols-1))/2+0.5];
+        node(i,j).v = [0 0];
+        if i == 1
+            node(i,j).isFixed = true;
+        else
+            node(i,j).isFixed = false;
+        end
+        
+    end
+end
+
+% Initial time
+t = 0;
+
+while t < 5
+    clf
+    hold on
+    xlim([0 1])
+    ylim([0 1])
+    for i = 1:rows
+        for j = 1:cols
+            if node(i,j).isFixed == false
+                f1 = [0 0];
+                f2 = [0 0];
+                f3 = [0 0];
+                f4 = [0 0];
+                f5 = [0 0];
+                f6 = [0 0];
+                f7 = [0 0];
+                f8 = [0 0];
+                L = 0.5/(cols-1)-0.1;
+                L2 = sqrt(L^2 + L^2);
+                % Distances between nodes
+                if i > 1 && j > 1
+                    x1 = node(i,j).p-node(i-1,j-1).p;
+                    X1 = norm(x1);
+                    f1 = -k*(X1-L)*x1/X1;
+                end
+                if j > 1
+                    x2 = node(i,j).p-node(i,j-1).p;
+                    X2 = norm(x2);
+                    f2 = -k*(X2-L)*x2/X2;
+                end
+                if i < rows && j > 1
+                    x3 = node(i,j).p-node(i+1,j-1).p;
+                    X3 = norm(x3);
+                    f3 = -k*(X3-L)*x3/X3;
+                end
+                if i < rows
+                    x4 = node(i,j).p-node(i+1,j).p;
+                    X4 = norm(x4);
+                    f4 = -k*(X4-L)*x4/X4;
+                end
+                if i < rows && j < cols
+                    x5 = node(i,j).p-node(i+1,j+1).p;
+                    X5 = norm(x5);
+                    f5 = -k*(X5-L)*x5/X5;
+                end
+                if j < cols
+                    x6 = node(i,j).p-node(i,j+1).p; %
+                    X6 = norm(x6);
+                    f6 = -k*(X6-L)*x6/X6;
+                end
+                if i > 1 && j < cols
+                    x7 = node(i,j).p-node(i-1,j+1).p;
+                    X7 = norm(x7);
+                    f7 = -k*(X7-L)*x7/X7;
+                end
+                if i > 1
+                    x8 = node(i,j).p-node(i-1,j).p;
+                    X8 = norm(x8);
+                    f8 = -k*(X8-L)*x8/X8;
+                end
+                f_d  = node(i,j).v*c;
+                node(i,j).f_sum = f1+f2+f3+f4+f5+f6+f7+f8-f_g-f_d;
+                % Calc acceleration
+                node(i,j).a = 1/m * node(i,j).f_sum;
+                % Numerical method to get p
+                node(i,j) = simpleEuler(node(i,j),h);
+
+                % Check if outside borders
+                if node(i,j).p(2) < 0
+                    node(i,j).v(2) = -1*(node(i,j).v(2));
+                    node(i,j).p(2) = 0;
+                elseif node(i,j).p(2) > 1
+                    node(i,j).v(2) = -1*(node(i,j).v(2));
+                    node(i,j).p(2) = 1;
+                end
+                if node(i,j).p(1) < 0
+                    node(i,j).v(1) = -1*(node(i,j).v(1));
+                    node(i,j).p(1) = 0;
+                elseif node(i,j).p(1) > 1
+                    node(i,j).v(1) = -1*(node(i,j).v(1));
+                    node(i,j).p(1) = 1;
+                end
+            end
+        end
+    end
+
+    % Diff between velocities
+    %     v1 = node(1).v-node(2).v;
+    %     v2 = node(2).v-node(3).v;
+    %     v3 = node(3).v-node(4).v;
+    %     v4 = node(4).v-node(1).v;
+    %     v5 = node(1).v-node(3).v; % diagonal velocity
+    %     v6 = node(2).v-node(4).v; % -- || --
+
+
+    % Damping forces
+    %     fd12 = c*v1;
+    %     fd23 = c*v2;
+    %     fd34 = c*v3;
+    %     fd41 = c*v4;
+    %     fd13 = c*v5;
+    %     fd24 = c*v6;
+
+    for i = 1:rows
+        for j = 1:cols
+            plot(node(i,j).p(1), node(i,j).p(2), ".b", 'MarkerSize', 20)
+            if i > 1 && j > 1
+                plot([node(i,j).p(1) node(i-1,j-1).p(1)], [node(i,j).p(2) node(i-1,j-1).p(2)])
+            end
+            if j > 1
+                plot([node(i,j).p(1) node(i,j-1).p(1)], [node(i,j).p(2) node(i,j-1).p(2)])
+            end
+            if i < rows
+                plot([node(i,j).p(1) node(i+1,j).p(1)], [node(i,j).p(2) node(i+1,j).p(2)])
+            end
+            %             if i > 1 && j < cols
+            %                 plot([node(i,j).p(1) node(i-1,j+1).p(1)], [node(i,j).p(2) node(i-1,j+1).p(2)])
+            %             end
+        end
+    end
+
+
+    pause(h)
+    t = t+h;
+
+end
---
+++ MATLAB/simple4mass_2.m
@@ -0,0 +1,108 @@
+clear
+clf
+hold on
+xlim([0 1])
+ylim([0 1])
+
+h = 0.01;
+% Masses
+m = [1 1 1 1];
+% Spring constant
+k = 30;
+k2 = 10;
+% Damping constant
+c = 5;
+% Initial positions
+node(1).p = [0.5 0.1];
+node(2).p = [0.1 0.1];
+node(3).p = [0.1 0.5];
+node(4).p = [0.5 0.5];
+% Initial velocities
+for i = 1:4
+    node(i).v = [0 0];
+end
+% Initial time
+t = 0;
+while t < 5
+    clf
+    hold on
+    xlim([0 1])
+    ylim([0 1])
+    % Distances between nodes
+    x1 = node(1).p-node(2).p;
+    X1 = norm(x1);
+    x2 = node(2).p-node(3).p;
+    X2 = norm(x2);
+    x3 = node(3).p-node(4).p;
+    X3 = norm(x3);
+    x4 = node(4).p-node(1).p;
+    X4 = norm(x4);
+    x5 = node(1).p-node(3).p; % diagonal force
+    X5 = norm(5);
+    x6 = node(2).p-node(4).p; % -- || --
+    X6 = norm(6);
+    L = 0.4;
+    L2 = sqrt(0.4^2 + 0.4^2);
+    % Diff between velocities
+    v1 = node(1).v-node(2).v;
+    v2 = node(2).v-node(3).v;
+    v3 = node(3).v-node(4).v;
+    v4 = node(4).v-node(1).v;
+    v5 = node(1).v-node(3).v; % diagonal velocity
+    v6 = node(2).v-node(4).v; % -- || --
+    % Forces between nodes
+    f12 = k*(X1-L)*x1/X1;
+    f23 = k*(X2-L)*x2/X2;
+    f34 = k*(X3-L)*x3/X3;
+    f41 = k*(X4-L)*x4/X4;
+    f13 = k2*(X5-L2)*x5/X5; % diagonal force
+    f24 = k2*(X6-L2)*x6/X6; % -- || --
+    f_g = [0 9.82];
+    % Damping forces
+    fd12 = c*v1;
+    fd23 = c*v2;
+    fd34 = c*v3;
+    fd41 = c*v4;
+    fd13 = c*v5;
+    fd24 = c*v6;
+    % Sum of forces for each node
+    node(1).f_sum = -f_g - f12 + f41 - f13 - (fd12 - fd41 + fd13);
+    node(2).f_sum = -f_g + f12 - f23 - f24 - (-fd12 + fd23 + fd24);
+    node(3).f_sum = -f_g + f23 - f34 + f13 - (-f23 + f34 - fd13);
+    node(4).f_sum = -f_g + f34 - f41 + f24 - (-f34 + f41 - fd24);
+    % Numerical method for each nodes acceleration to position
+    for i = 1:4
+        node(i).a = 1/m(i) * node(i).f_sum;
+        node(i) = simpleEuler(node(i),h);
+    end
+
+    for i = 1:4
+        % Check if inside borders
+        if node(i).p(2) < 0
+            node(i).v(2) = -1*(node(i).v(2));
+            node(i).p(2) = 0;
+        elseif node(i).p(2) > 1
+            node(i).v(2) = -1*(node(i).v(2));
+            node(i).p(2) = 1;
+        end
+        if node(i).p(1) < 0
+            node(i).v(1) = -1*(node(i).v(1));
+            node(i).p(1) = 0;
+        elseif node(i).p(1) > 1
+            node(i).v(1) = -1*(node(i).v(1));
+            node(i).p(1) = 1;
+        end
+    end
+
+    for i = 1:4
+        x_plot(i) = node(i).p(1);
+        y_plot(i) = node(i).p(2);
+    end
+    x_plot(4+1) = node(1).p(1);
+    y_plot(4+1) = node(1).p(2);
+    plot(x_plot, y_plot, ".b", 'MarkerSize', 20)
+    plot(x_plot, y_plot, "b");
+    pause(h)
+    t = t+h;
+
+end
