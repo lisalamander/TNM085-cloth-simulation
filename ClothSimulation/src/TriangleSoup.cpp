28ca0752296ff252c83b4c1b41161e87fe6ba185
Alfred Rundqvist <alfred.rundqvist@gmail.com>
2022-03-14 15:09:08 +0100

    Release

--- ClothSimulation/src/TriangleSoup.cpp
+++ ClothSimulation/src/TriangleSoup.cpp
@@ -394,212 +394,212 @@ void TriangleSoup::createSphere(float radius, int segments) {
  *
  * Author: Stefan Gustavson (stegu@itn.liu.se) 2014.
  * This code is in the public domain.
- */
-void TriangleSoup::readOBJ(const std::string& filename) {
-    FILE* objfile = fopen(filename.c_str(), "r");
-
-    if (!objfile) {
-        std::cerr << "File not found: " << filename << "\n";
-        return;
-    }
-
-    // Scan through the file to count the number of data elements
-    char line[256];
-    char tag[3];
-
-    int numverts = 0;
-    int numnormals = 0;
-    int numtexcoords = 0;
-    int numfaces = 0;
-    while (fgets(line, 256, objfile)) {
-        sscanf(line, "%2s ", tag);
-        if (!strcmp(tag, "v")) {
-            numverts++;
-        } else if (!strcmp(tag, "vn")) {
-            numnormals++;
-        } else if (!strcmp(tag, "vt")) {
-            numtexcoords++;
-        } else if (!strcmp(tag, "f")) {
-            numfaces++;
-        }
-        // else {
-        //     std::cout << "Ignoring line starting with \"" << tag << "\"\n";
-        // }
-    }
-
-    std::cout << "loadObj(\"" << filename << "\"): found " << numverts << " vertices, "
-              << numnormals << " normals, " << numtexcoords << " texcoords, " << numfaces
-              << " faces.\n";
-
-    std::vector<float> verts(3 * numverts);
-    std::vector<float> normals(3 * numnormals);
-    std::vector<float> texcoords(2 * numtexcoords);
-
-    vertexarray_.resize(8 * 3 * numfaces);
-    indexarray_.resize(3 * numfaces);
-    nverts_ = 3 * numfaces;
-    ntris_ = numfaces;
-
-    rewind(objfile);  // Start from the top again to read data
-
-    int i_v = 0;
-    int i_n = 0;
-    int i_t = 0;
-    int i_f = 0;
-
-    int readerror = 0;
-    while (fgets(line, 256, objfile)) {
-        tag[0] = '\0';
-        sscanf(line, "%2s ", tag);
-        if (!strcmp(tag, "v")) {
-            // A vertex with three coordinates
-            // std::cout << "Reading vertex " << i_v + 1 << "\n";
-
-            int numargs = sscanf(line, "v %f %f %f", &verts[3 * i_v], &verts[3 * i_v + 1],
-                                 &verts[3 * i_v + 2]);
-            if (numargs != 3) {
-                std::cerr << "Malformed vertex data found at vertex " << i_v + 1 << "\nAborting\n";
-                readerror = 1;
-                break;
-            }
-            i_v++;
-        } else if (!strcmp(tag, "vn")) {
-            // A vertex normal with three components
-            // std::cout << "Reading normal " << i_n + 1 << "\n";
-
-            int numargs = sscanf(line, "vn %f %f %f", &normals[3 * i_n], &normals[3 * i_n + 1],
-                                 &normals[3 * i_n + 2]);
-            if (numargs != 3) {
-                std::cerr << "Malformed normal data found at normal" << i_n + 1 << "\nAborting\n";
-                readerror = 1;
-                break;
-            }
-            i_n++;
-        } else if (!strcmp(tag, "vt")) {
-            // A vertex texture coordinate, two components
-            // std::cout << "Reading texcoord " << i_t + 1 << "\n";
-
-            int numargs = sscanf(line, "vt %f %f", &texcoords[2 * i_t], &texcoords[2 * i_t + 1]);
-            if (numargs != 2) {
-                std::cerr << "Malformed texcoord data found at texcoord " << i_t + 1
-                          << "\nAborting\n";
-                readerror = 1;
-                break;
-            }
-            i_t++;
-        } else if (!strcmp(tag, "f")) {
-            // A face with three or more vertex indices
-            // std::cout << "Reading face " << i_f + 1 << "\n";
-
-            int v1, v2, v3, n1, n2, n3, t1, t2, t3;
-            int numargs = sscanf(line, "f %d/%d/%d %d/%d/%d %d/%d/%d", &v1, &t1, &n1, &v2, &t2, &n2,
-                                 &v3, &t3, &n3);
-            if (numargs != 9) {  // Accept only triangles. Quads cause an error.
-                std::cerr << "Malformed face data found at vertex " << i_f + 1 << "\nAborting\n";
-                readerror = 1;
-                break;
-            }
-            //			printf("Read vertex data %d/%d/%d %d/%d/%d %d/%d/%d\n",
-            //			v1, t1, n1, v2, t2, n2, v3, t3, n3);
-            // Indices in OBJ files start at 1, but C++ arrays start at index 0.
-            --v1;
-            --v2;
-            --v3;
-            --n1;
-            --n2;
-            --n3;
-            --t1;
-            --t2;
-            --t3;
-
-            const int currentv = 8 * 3 * i_f;
-            vertexarray_[currentv] = verts[3 * v1];
-            vertexarray_[currentv + 1] = verts[3 * v1 + 1];
-            vertexarray_[currentv + 2] = verts[3 * v1 + 2];
-            vertexarray_[currentv + 3] = normals[3 * n1];
-            vertexarray_[currentv + 4] = normals[3 * n1 + 1];
-            vertexarray_[currentv + 5] = normals[3 * n1 + 2];
-            vertexarray_[currentv + 6] = texcoords[2 * t1];
-            vertexarray_[currentv + 7] = texcoords[2 * t1 + 1];
-            vertexarray_[currentv + 8] = verts[3 * v2];
-            vertexarray_[currentv + 9] = verts[3 * v2 + 1];
-            vertexarray_[currentv + 10] = verts[3 * v2 + 2];
-            vertexarray_[currentv + 11] = normals[3 * n2];
-            vertexarray_[currentv + 12] = normals[3 * n2 + 1];
-            vertexarray_[currentv + 13] = normals[3 * n2 + 2];
-            vertexarray_[currentv + 14] = texcoords[2 * t2];
-            vertexarray_[currentv + 15] = texcoords[2 * t2 + 1];
-            vertexarray_[currentv + 16] = verts[3 * v3];
-            vertexarray_[currentv + 17] = verts[3 * v3 + 1];
-            vertexarray_[currentv + 18] = verts[3 * v3 + 2];
-            vertexarray_[currentv + 19] = normals[3 * n3];
-            vertexarray_[currentv + 20] = normals[3 * n3 + 1];
-            vertexarray_[currentv + 21] = normals[3 * n3 + 2];
-            vertexarray_[currentv + 22] = texcoords[2 * t3];
-            vertexarray_[currentv + 23] = texcoords[2 * t3 + 1];
-            indexarray_[3 * i_f] = 3 * i_f;
-            indexarray_[3 * i_f + 1] = 3 * i_f + 1;
-            indexarray_[3 * i_f + 2] = 3 * i_f + 2;
-            i_f++;
-        }
-    }
-
-    // Clean up the temporary arrays we created
-    fclose(objfile);
-
-    if (readerror) {  // Delete corrupt data and bail out if a read error occured
-        std::cerr << "Mesh read error: No mesh data generated\n";
-        clean();
-        return;
-    }
-
-    // Generate one vertex array object (VAO) and bind it
-    glGenVertexArrays(1, &vao_);
-    glBindVertexArray(vao_);
-
-    // Generate two buffer IDs
-    glGenBuffers(1, &vertexbuffer_);
-    glGenBuffers(1, &indexbuffer_);
-
-    // Activate the vertex buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_);
-    // Present our vertex coordinates to OpenGL
-    glBufferData(GL_ARRAY_BUFFER, vertexarray_.size() * sizeof(GLfloat), vertexarray_.data(),
-                 GL_STATIC_DRAW);
-
-    // Specify how many attribute arrays we have in our VAO
-    glEnableVertexAttribArray(0);  // Vertex coordinates
-    glEnableVertexAttribArray(1);  // Normals
-    glEnableVertexAttribArray(2);  // Texture coordinates
-    // Specify how OpenGL should interpret the vertex buffer data:
-    // Attributes 0, 1, 2 (must match the lines above and the layout in the shader)
-    // Number of dimensions (3 means vec3 in the shader, 2 means vec2)
-    // Type GL_FLOAT
-    // Not normalized (GL_FALSE)
-    // Stride 8 (interleaved array with 8 floats per vertex)
-    // Array buffer offset 0, 3, 6 (offset into first vertex)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
-                          (void*)0);  // xyz coordinates
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
-                          (void*)(3 * sizeof(GLfloat)));  // normals
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
-                          (void*)(6 * sizeof(GLfloat)));  // texcoords
-
-    // Activate the index buffer
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer_);
-    // Present our vertex indices to OpenGL
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexarray_.size() * sizeof(GLuint), indexarray_.data(),
-                 GL_STATIC_DRAW);
-
-    // Deactivate (unbind) the VAO and the buffers again.
-    // Do NOT unbind the buffers while the VAO is still bound.
-    // The index buffer is an essential part of the VAO state.
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    return;
-}
+// */
+//void TriangleSoup::readOBJ(const std::string& filename) {
+//    FILE* objfile = fopen(filename.c_str(), "r");
+//
+//    if (!objfile) {
+//        std::cerr << "File not found: " << filename << "\n";
+//        return;
+//    }
+//
+//    // Scan through the file to count the number of data elements
+//    char line[256];
+//    char tag[3];
+//
+//    int numverts = 0;
+//    int numnormals = 0;
+//    int numtexcoords = 0;
+//    int numfaces = 0;
+//    while (fgets(line, 256, objfile)) {
+//        sscanf(line, "%2s ", tag);
+//        if (!strcmp(tag, "v")) {
+//            numverts++;
+//        } else if (!strcmp(tag, "vn")) {
+//            numnormals++;
+//        } else if (!strcmp(tag, "vt")) {
+//            numtexcoords++;
+//        } else if (!strcmp(tag, "f")) {
+//            numfaces++;
+//        }
+//        // else {
+//        //     std::cout << "Ignoring line starting with \"" << tag << "\"\n";
+//        // }
+//    }
+//
+//    std::cout << "loadObj(\"" << filename << "\"): found " << numverts << " vertices, "
+//              << numnormals << " normals, " << numtexcoords << " texcoords, " << numfaces
+//              << " faces.\n";
+//
+//    std::vector<float> verts(3 * numverts);
+//    std::vector<float> normals(3 * numnormals);
+//    std::vector<float> texcoords(2 * numtexcoords);
+//
+//    vertexarray_.resize(8 * 3 * numfaces);
+//    indexarray_.resize(3 * numfaces);
+//    nverts_ = 3 * numfaces;
+//    ntris_ = numfaces;
+//
+//    rewind(objfile);  // Start from the top again to read data
+//
+//    int i_v = 0;
+//    int i_n = 0;
+//    int i_t = 0;
+//    int i_f = 0;
+//
+//    int readerror = 0;
+//    while (fgets(line, 256, objfile)) {
+//        tag[0] = '\0';
+//        sscanf(line, "%2s ", tag);
+//        if (!strcmp(tag, "v")) {
+//            // A vertex with three coordinates
+//            // std::cout << "Reading vertex " << i_v + 1 << "\n";
+//
+//            int numargs = sscanf(line, "v %f %f %f", &verts[3 * i_v], &verts[3 * i_v + 1],
+//                                 &verts[3 * i_v + 2]);
+//            if (numargs != 3) {
+//                std::cerr << "Malformed vertex data found at vertex " << i_v + 1 << "\nAborting\n";
+//                readerror = 1;
+//                break;
+//            }
+//            i_v++;
+//        } else if (!strcmp(tag, "vn")) {
+//            // A vertex normal with three components
+//            // std::cout << "Reading normal " << i_n + 1 << "\n";
+//
+//            int numargs = sscanf(line, "vn %f %f %f", &normals[3 * i_n], &normals[3 * i_n + 1],
+//                                 &normals[3 * i_n + 2]);
+//            if (numargs != 3) {
+//                std::cerr << "Malformed normal data found at normal" << i_n + 1 << "\nAborting\n";
+//                readerror = 1;
+//                break;
+//            }
+//            i_n++;
+//        } else if (!strcmp(tag, "vt")) {
+//            // A vertex texture coordinate, two components
+//            // std::cout << "Reading texcoord " << i_t + 1 << "\n";
+//
+//            int numargs = sscanf(line, "vt %f %f", &texcoords[2 * i_t], &texcoords[2 * i_t + 1]);
+//            if (numargs != 2) {
+//                std::cerr << "Malformed texcoord data found at texcoord " << i_t + 1
+//                          << "\nAborting\n";
+//                readerror = 1;
+//                break;
+//            }
+//            i_t++;
+//        } else if (!strcmp(tag, "f")) {
+//            // A face with three or more vertex indices
+//            // std::cout << "Reading face " << i_f + 1 << "\n";
+//
+//            int v1, v2, v3, n1, n2, n3, t1, t2, t3;
+//            int numargs = sscanf(line, "f %d/%d/%d %d/%d/%d %d/%d/%d", &v1, &t1, &n1, &v2, &t2, &n2,
+//                                 &v3, &t3, &n3);
+//            if (numargs != 9) {  // Accept only triangles. Quads cause an error.
+//                std::cerr << "Malformed face data found at vertex " << i_f + 1 << "\nAborting\n";
+//                readerror = 1;
+//                break;
+//            }
+//            //			printf("Read vertex data %d/%d/%d %d/%d/%d %d/%d/%d\n",
+//            //			v1, t1, n1, v2, t2, n2, v3, t3, n3);
+//            // Indices in OBJ files start at 1, but C++ arrays start at index 0.
+//            --v1;
+//            --v2;
+//            --v3;
+//            --n1;
+//            --n2;
+//            --n3;
+//            --t1;
+//            --t2;
+//            --t3;
+//
+//            const int currentv = 8 * 3 * i_f;
+//            vertexarray_[currentv] = verts[3 * v1];
+//            vertexarray_[currentv + 1] = verts[3 * v1 + 1];
+//            vertexarray_[currentv + 2] = verts[3 * v1 + 2];
+//            vertexarray_[currentv + 3] = normals[3 * n1];
+//            vertexarray_[currentv + 4] = normals[3 * n1 + 1];
+//            vertexarray_[currentv + 5] = normals[3 * n1 + 2];
+//            vertexarray_[currentv + 6] = texcoords[2 * t1];
+//            vertexarray_[currentv + 7] = texcoords[2 * t1 + 1];
+//            vertexarray_[currentv + 8] = verts[3 * v2];
+//            vertexarray_[currentv + 9] = verts[3 * v2 + 1];
+//            vertexarray_[currentv + 10] = verts[3 * v2 + 2];
+//            vertexarray_[currentv + 11] = normals[3 * n2];
+//            vertexarray_[currentv + 12] = normals[3 * n2 + 1];
+//            vertexarray_[currentv + 13] = normals[3 * n2 + 2];
+//            vertexarray_[currentv + 14] = texcoords[2 * t2];
+//            vertexarray_[currentv + 15] = texcoords[2 * t2 + 1];
+//            vertexarray_[currentv + 16] = verts[3 * v3];
+//            vertexarray_[currentv + 17] = verts[3 * v3 + 1];
+//            vertexarray_[currentv + 18] = verts[3 * v3 + 2];
+//            vertexarray_[currentv + 19] = normals[3 * n3];
+//            vertexarray_[currentv + 20] = normals[3 * n3 + 1];
+//            vertexarray_[currentv + 21] = normals[3 * n3 + 2];
+//            vertexarray_[currentv + 22] = texcoords[2 * t3];
+//            vertexarray_[currentv + 23] = texcoords[2 * t3 + 1];
+//            indexarray_[3 * i_f] = 3 * i_f;
+//            indexarray_[3 * i_f + 1] = 3 * i_f + 1;
+//            indexarray_[3 * i_f + 2] = 3 * i_f + 2;
+//            i_f++;
+//        }
+//    }
+//
+//    // Clean up the temporary arrays we created
+//    fclose(objfile);
+//
+//    if (readerror) {  // Delete corrupt data and bail out if a read error occured
+//        std::cerr << "Mesh read error: No mesh data generated\n";
+//        clean();
+//        return;
+//    }
+//
+//    // Generate one vertex array object (VAO) and bind it
+//    glGenVertexArrays(1, &vao_);
+//    glBindVertexArray(vao_);
+//
+//    // Generate two buffer IDs
+//    glGenBuffers(1, &vertexbuffer_);
+//    glGenBuffers(1, &indexbuffer_);
+//
+//    // Activate the vertex buffer
+//    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_);
+//    // Present our vertex coordinates to OpenGL
+//    glBufferData(GL_ARRAY_BUFFER, vertexarray_.size() * sizeof(GLfloat), vertexarray_.data(),
+//                 GL_STATIC_DRAW);
+//
+//    // Specify how many attribute arrays we have in our VAO
+//    glEnableVertexAttribArray(0);  // Vertex coordinates
+//    glEnableVertexAttribArray(1);  // Normals
+//    glEnableVertexAttribArray(2);  // Texture coordinates
+//    // Specify how OpenGL should interpret the vertex buffer data:
+//    // Attributes 0, 1, 2 (must match the lines above and the layout in the shader)
+//    // Number of dimensions (3 means vec3 in the shader, 2 means vec2)
+//    // Type GL_FLOAT
+//    // Not normalized (GL_FALSE)
+//    // Stride 8 (interleaved array with 8 floats per vertex)
+//    // Array buffer offset 0, 3, 6 (offset into first vertex)
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+//                          (void*)0);  // xyz coordinates
+//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+//                          (void*)(3 * sizeof(GLfloat)));  // normals
+//    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+//                          (void*)(6 * sizeof(GLfloat)));  // texcoords
+//
+//    // Activate the index buffer
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer_);
+//    // Present our vertex indices to OpenGL
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexarray_.size() * sizeof(GLuint), indexarray_.data(),
+//                 GL_STATIC_DRAW);
+//
+//    // Deactivate (unbind) the VAO and the buffers again.
+//    // Do NOT unbind the buffers while the VAO is still bound.
+//    // The index buffer is an essential part of the VAO state.
+//    glBindVertexArray(0);
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+//
+//    return;
+//}
 
 /* Print data from a TriangleSoup object, for debugging purposes */
 void TriangleSoup::print() {
